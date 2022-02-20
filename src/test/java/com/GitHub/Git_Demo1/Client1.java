9bc50a09232a462ffb63c6ec1dce3df7643fb321
SwaradaSuratekar <swaradasuratekar203@gmail.com>
2022-02-20 18:29:05 +0530

    First commit

---
+++ src/test/java/com/GitHub/Git_Demo1/Client1.java
@@ -0,0 +1,11 @@
+package com.GitHub.Git_Demo1;
+
+public class Client1 {
+	String m="MobleCiti";
+	
+	public static void main(String[] args) {
+		Client1 c=new Client1();
+		System.out.println(c.m);
+	}
+
+}
---
+++ src/test/java/com/GitHub/Git_Demo1/Client2.java
@@ -0,0 +1,11 @@
+package com.GitHub.Git_Demo1;
+
+public class Client2 {
+static String p="Ventrix";
+
+	public static void main(String[] args) {
+		System.out.println(p);
+
+	}
+
+}
